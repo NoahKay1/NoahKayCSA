10285ec888d56cd45b839d319a0326636de0cc08
NoahKay1 <KayN2930@sduhsd.lan>
2018-02-21 11:01:09 -0800

    Working on Unit 8

---
+++ Unit8/src/Lab14d.java
@@ -0,0 +1,16 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab14d
+{
+	public static void main( String args[] )
+	{
+		Grades g = new Grades(5, new double[]{100, 90, 85, 72.5, 95.6});
+		System.out.println(g);
+		
+		
+	}
+}
