b73d1879b0b19ffe21354ed9450a2c03c83ec0b1
NoahKay1 <KayN2930@sduhsd.lan>
2018-02-28 11:55:38 -0800

    Working on Unit 9

--- Unit9/src/DeckTester.java
+++ Unit9/src/DeckTester.java
@@ -8,6 +8,15 @@ public class DeckTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		int[] vals = new int[] {1,2,3,4,5,6,1,2,3};
+		String[] suits = new String[] {"Spade", "Spade", "Clubs", "Clubs", "Diamonds", "Diamonds", "Hearts", "Hearts", "Test"};
+		String[] ranks = new String [] {"One", "Two", "Three", "Four", "Five", "Six", "One", "Two", "Three"};
+		
+		Deck d1 = new Deck(ranks, suits, vals);
+		System.out.println(d1);
+		d1.deal();
+		System.out.println(d1);
+		d1.deal();
+		System.out.println(d1);
 	}
 }
