c5e05583011d7e1316d2ead108e253d56fdb95cc
NoahKay1 <36010627+NoahKay1@users.noreply.github.com>
2018-03-29 23:52:28 -0400

    Working on unit 13

--- Unit13/src/QuickSort.java
+++ Unit13/src/QuickSort.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class - 
@@ -11,43 +11,42 @@ public class QuickSort
 {
 	private static int passCount;
 
+	public static void zeroPassCount() {passCount = 0;}
+	
+	@SuppressWarnings("rawtypes")
 	public static void quickSort(Comparable[] list)
 	{
-
-
-
-
+		quickSort(list, 0, list.length-1);
 	}
 
 
+	@SuppressWarnings("rawtypes")
 	private static void quickSort(Comparable[] list, int low, int high)
 	{
-
-
-
-
-
-
-
+		if(low < high) {
+			int split = partition(list, low, high);
+			out.println("pass " + passCount++ + " " + Arrays.toString(list));
+			quickSort(list, low, split);
+			quickSort(list, split+1, high);
+		}
 	}
 
 
+	@SuppressWarnings({"rawtypes", "unchecked"})
 	private static int partition(Comparable[] list, int low, int high)
 	{
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		Comparable pivot = list[low];
+		int bot = low-1;
+		int top = high+1;
+		while(bot < top) {
+			while(list[--top].compareTo(pivot) > 0);
+			while(list[++bot].compareTo(pivot) < 0);
+			if(bot>=top)
+				return top;
+			Comparable temp = list[bot];
+			list[bot] = list[top];
+			list[top] = temp;
+		} 
 		return 0;
 	}
 }
